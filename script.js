6ccce58376466509de9a4033d1df024c923c7a22
nasim-k-t <abdulnasimkt@gmail.com>
2023-08-30 09:52:44 +0530

    Not Done

--- script.js
+++ script.js
@@ -47,19 +47,71 @@ function passwordVisibility() {
     }
 }
 function si_validate() {
-    if (document.getElementById("si-name").value.length === 0) {
-        document.getElementById("si-name").style.border = "1px solid red";
-        document.getElementById("err_name").style.display = "block";
-        //return false;
+    var fields = [
+        { id: "si-name", err_id: "err_name" },
+        { id: "si-email", err_id: "err_email" },
+        { id: "si-pas", err_id: "err_pass" }
+    ];
+
+    var isValid = true;
+
+    fields.forEach(function (field) {
+        var inputElement = document.getElementById(field.id);
+        var errorElement = document.getElementById(field.err_id);
+
+        if (inputElement.value.length === 0) {
+            inputElement.style.border = "1px solid red";
+            errorElement.style.display = "block";
+            isValid = false;
+        } else {
+            inputElement.style.border = "none";
+            errorElement.style.display = "none";
+        }
+    });
+
+    return isValid;
+}
+
+
+function validate() {
+    var fields = [
+        { id: "name", err_id: "er_name" },
+        { id: "email", err_id: "er_email" },
+        { id: "pas", err_id: "er_pas" },
+        { id: "org", err_id: "er_org" },
+        { id: "summary", err_id: "er_summary" },
+        { id: "con-pas", err_id: "er_con-pas" }
+    ];
+
+    var isValid = true;
+
+    fields.forEach(function (field) {
+        var inputElement = document.getElementById(field.id);
+        var errorElement = document.getElementById(field.err_id);
+
+        if (inputElement.value.length === 0) {
+            inputElement.style.border = "1px solid red";
+            errorElement.style.display = "block";
+            isValid = false;
+        } else {
+            inputElement.style.border = "none";
+            errorElement.style.display = "none";
+        }
+    });
+    var pasInput = document.getElementById('pas');
+    var conPasInput = document.getElementById('con-pas');
+    
+    if (pasInput.value !== conPasInput.value) {
+        pasInput.style.border = "1px solid red";
+        conPasInput.style.border = "1px solid red";
+        document.getElementById("pas-match").style.display = "block";
+        isValid = false;
+    } else {
+        pasInput.style.border = "none";
+        conPasInput.style.border = "none";
+        document.getElementById("pas-match").style.display = "none";
     }
-    if (document.getElementById("si-email").value.length === 0) {
-        document.getElementById("si-email").style.border = "1px solid red";
-        document.getElementById("err_email").style.display = "block";
-        //return false;
-    }
-    if (document.getElementById("si-pas").value.length === 0) {
-        document.getElementById("si-pas").style.border = "1px solid red";
-        document.getElementById("err_pas").style.display = "block";
-        //return false;
-    }
+    
+
+    return isValid;
 }
